7afdc9a21c291ea464e99a70bedda2f70258b9d6
Meek Karan <meekkaran@gmail.com>
2021-01-30 12:55:36 +0300

    footer

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./Photos.css";
 
 function Photos() {
     return (
         <div>
-            <p>what we prepare</p>
-            <div className="images wrapper">
-                <img src="../images/chapo.jpg" />
+            <h1>What we prepare</h1>
+            <div className="imagewrap">
+                <div className="image">
+                   <img src="../images/fish.jpg" />
+                   <img src="../images/beef.jpg" />
+                   <img src="../images/chicken.jpg"/>
+                </div>
+                <div className="image">
+                   <img src="../images/chapo.jpg" />
+                   <img src="../images/bhajia.jpg"/>
+                   <img src="../images/pilau.jpg" />
+                </div>
+                <div className="image">
+                   <img src="../images/beer.jpg" />
+                   <img src="../images/smoothie.jpg"/>
+                   <img src="../images/milk.jpg" />
+                </div>
+                
             </div>
         </div>
     )
